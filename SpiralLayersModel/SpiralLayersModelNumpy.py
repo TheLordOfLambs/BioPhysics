98cf9e7fd6428c287a6891fa590541791b42b518
NemoMalhomme <73847316+NemoMalhomme@users.noreply.github.com>
2020-12-12 16:05:51 +0100

    Update SpiralLayersModelNumpy.py

--- SpiralLayersModel/SpiralLayersModelNumpy.py
+++ SpiralLayersModel/SpiralLayersModelNumpy.py
@@ -84,7 +84,7 @@ def plot_graphs(R,d,L):
 #Constants; R,l,b are in nm
 R = 20
 L = 19000*.33
-kb = 1.38e-23
+kb = 1.38e-23*1e18 #Because we're in nm and not in meters
 T = 300
 b = 50 #correlation lenght of DNA, 50 nm
 
@@ -97,7 +97,7 @@ b = 50 #correlation lenght of DNA, 50 nm
 from scipy.optimize import curve_fit
 
 #I haven't put any security in case the data has values of L inserted too big for the model, because in practive we're very far from there.
-def model(x, d, kbTbpi):
+def model(x, d, kbTbpi):#, R):
     lengths,energies=packing_energy(R, d, math.inf, factor = kbTbpi)
     forces = energy_to_force(lengths, energies)
     
@@ -111,6 +111,9 @@ def model(x, d, kbTbpi):
     #print(data_x_indexes)
     return forces[data_x_indexes]
 
+def model1param(x, d):
+    return model (x, d, 500)
+
 def draw_me_a_fit(datax, datay, model):
     popt, pcov = curve_fit(model, datax, datay)
     x = L*datax/100
@@ -118,18 +121,25 @@ def draw_me_a_fit(datax, datay, model):
     
     
     plt.plot(x, datay, 'b+', label='data')
-    plt.plot(x, y, 'r--', label='fit: d=%5.3f, factor=%5.3f' % tuple(popt))
+    plt.plot(x, y, 'r--', label='fit: d=%5.3f, factor: %5.3f' % tuple(popt))
 
     plt.xlabel('Length inserted in the capsid (nm)')
-    plt.ylabel('Force (idk)') 
-    plt.title('Fit of the Spiral-Layers model')
+    plt.ylabel('Force (pN)') 
+    plt.title('Fit of the Spiral-Layers bending model')
     plt.legend()
     plt.show()
+    
+    return x, y, popt
+
+def draw_everything(datax, datay, model, r, d):
+    x, y, params = draw_me_a_fit(datax, datay, model)
+    return 0
+
+
+
     
 datax = np.array([30.37292789969,31.99187600139,33.6108241031,35.22977220481,36.84872030651,38.46766840822,40.65324834552,42.27219644723,43.89114454894,45.51009265064,47.12904075235,48.74798885406,50.36693695576,51.98588505747,53.60483315918,55.22378126088,56.84272936259,58.4616774643,60.080625566,61.69957366771,63.31852176942,64.93746987113,66.55641797283,68.17536607454,69.79431417625,71.41326227795,73.03221037966,74.65115848137,76.27010658307,77.88905468478,79.50800278649,81.12695088819,82.7458989899,84.36484709161,85.98379519331,87.60274329502,89.22169139673,90.84063949843,92.45958760014,94.07853570185,95.69748380355,97.31643190526,98.93538000697,100.5543281087,102.1732762104,103.7922243121])
 
 datay = np.array([-0.5429885000496,-0.6086670962625,-0.1817562208784,0.01527956776048,0.01527956776048,-0.08323832655894,-0.08323832655894,0.1137974620799,0.4093511450382,1.033297809061,1.263172895806,1.460208684445,1.624405174978,1.952798156042,2.576744820065,3.200691484088,3.726120253792,4.842656389412,6.057710419352,7.502639536036,8.750532864082,9.867068999703,11.27915881828,13.15099881035,16.00801774561,18.17541142064,19.94873351839,21.49218052939,22.70723455933,24.11932437791,25.89264647566,28.15855804501,30.6543447011,33.47852433826,36.99232923565,39.84934817091,42.11525974026,43.92142113612,45.46486814712,46.81127936949,47.92781550511,48.88015515019,49.79965549717,50.52212005552,51.34310250818,51.9670491722])
     
-
-
-draw_me_a_fit(datax[:40], datay[:40], model)
+draw_me_a_fit(datax, datay, model)
